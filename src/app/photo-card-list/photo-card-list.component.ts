06020fc7e9525c99c99e8434e18aabde51745c3b
Valentin Vasilev <valentin.v1997@gmail.com>
2022-08-11 17:44:51 +0300

    Added shadows on the cards and hoover effects

---
+++ src/app/photo-card-list/photo-card-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-photo-card-list',
+  templateUrl: './photo-card-list.component.html',
+  styleUrls: ['./photo-card-list.component.css']
+})
+export class PhotoCardListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/photo-card/photo-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-photo-card',
+  templateUrl: './photo-card.component.html',
+  styleUrls: ['./photo-card.component.css']
+})
+export class PhotoCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
